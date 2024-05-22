62bacbe6135d1b2ffecdc96bdb5a0872d0b664b7
Diyorbek Ne'matov <diyorbeknematov0321@gmail.com>
2024-05-22 13:49:54 +0500

    Feature0branchda main.go ga o'zgartirish kiritildi

--- one/main.go
+++ one/main.go
@@ -1,5 +1,20 @@
 package main
 
+import "fmt"
+
+func addDigits(num int) int {
+    for num > 9 {
+        sum := 0
+        for num != 0 {
+            remainder := num % 10
+            sum += remainder
+            num /= 10
+        }
+        num = sum
+    }
+    return num
+}
+
 func main() {
-	
+	fmt.Println(addDigits(38))
 }
